83c16eb46bafa93552d62e25176283a3d5610224
mkjmkumar <mkjmkumar@rediffmail.com>
2016-08-16 14:25:29 +0530

    Falcon script to test falcon component using curl
    Falcon script to test falcon component using curl

---
+++ HDP20_FALCON.sh
@@ -0,0 +1,9 @@
+echo "curl http://127.0.1.1:15000/" >> output.txt
+
+echo "curl http://127.0.1.1:15000/"
+
+curl http://127.0.1.1:15000/
+
+echo "---> Validation for Falcon Installation done successfully..."
+
+echo "---> Validation for Falcon Installation done successfully..." >> output.txt
